0fef423f35efa2f0afe55d4bacddcafb581cee42
harriet.ballantyne <harriet.ballantyne@gmail.com>
2019-03-22 15:25:04 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -21,6 +21,7 @@ def while_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
   counter = 0
   while counter <= number_of_times
+  puts phrase
 end
 
 def until_iterator(number_of_times)
